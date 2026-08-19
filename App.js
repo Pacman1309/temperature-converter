9beb8c95fe3ec711f21119f4a0973b29104f3af7
Salvador Eliab Sagarnaga Rivera <superpacman13091@gmail.com>
2026-08-18 18:11:37 -0600

    issue-1 Added TemperatureScreen and React Native Paper

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import TemperatureScreen from './screens/TemperatureScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Temperature Converter</Text>
-      <Text style={styles.textoGrande}>Temperature Converter LETS GOOOO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <TemperatureScreen />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#5DC1B9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  titulo: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
-    marginBottom: 20,
-    textAlign: 'center',
-    paddingHorizontal: 20,
-    paddingTop: 20,
-  },
-  textoGrande: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
