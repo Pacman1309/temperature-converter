4645f0abe258482848c92026ef5ad76c11f55b98
Salvador Eliab Sagarnaga Rivera <superpacman13091@gmail.com>
2026-08-20 18:10:38 -0600

    Issue-1 added hook and changed screen to use hook

--- screens/TemperatureScreen.js
+++ screens/TemperatureScreen.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Button, Text, TextInput } from "react-native-paper";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button, Text, TextInput } from 'react-native-paper';
+import useTemperatureConverter from '../hooks/useTemperatureConverter';
 
 const TemperatureScreen = () => {
+  const { celsiusValue, setCelsiusValue, fahrenheitValue, convertTemperature } =
+    useTemperatureConverter();
+
   return (
     <SafeAreaView style={styles.container}>
       <Text variant="headlineMedium" style={styles.title}>
@@ -16,20 +20,27 @@ const TemperatureScreen = () => {
         style={styles.input}
         keyboardType="numeric"
         inputMode="decimal"
-        theme={{ colors: { primary: "#77F2DC" } }}
+        theme={{ colors: { primary: '#77F2DC' } }}
+        value={celsiusValue}
+        onChangeText={setCelsiusValue}
       />
       <Text style={styles.label}>Grados Fahrenheit:</Text>
       <TextInput
         id="fahrenheitResult"
         mode="flat"
-        value=""
+        value={fahrenheitValue}
         editable={false}
         selectTextOnFocus={false}
         style={styles.input}
       />
 
       <View style={styles.buttonWrapper}>
-        <Button id="convertButton" mode="contained" style={styles.button}>
+        <Button
+          id="convertButton"
+          mode="contained"
+          style={styles.button}
+          onPress={convertTemperature}
+        >
           CONVERTIR
         </Button>
       </View>
@@ -40,37 +51,37 @@ const TemperatureScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
     padding: 20,
   },
   title: {
     marginBottom: 16,
-    color: "#1f3a5f",
-    fontWeight: "700",
+    color: '#1f3a5f',
+    fontWeight: '700',
     fontSize: 20,
-    textAlign: "center",
+    textAlign: 'center',
   },
   label: {
     marginBottom: 8,
-    color: "#4b5d78",
+    color: '#4b5d78',
     fontSize: 15,
   },
   input: {
     marginBottom: 16,
-    backgroundColor: "#f9fbff",
+    backgroundColor: '#f9fbff',
   },
   celsiusInputFocused: {
-    borderColor: "#77F2DC",
+    borderColor: '#77F2DC',
   },
   buttonWrapper: {
-    alignItems: "center",
+    alignItems: 'center',
     marginTop: 8,
   },
   button: {
     borderRadius: 10,
-    width: "40%",
+    width: '40%',
     minWidth: 140,
-    backgroundColor: "#5005F2",
+    backgroundColor: '#5005F2',
   },
 });
 
